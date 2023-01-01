a17070f299259963356195bd0e1ac410e844513c
rukaiya <rukaiyaamin36@gmail.com>
2023-01-01 18:05:22 +0600

    admin registration

--- store/models.py
+++ store/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('-created', )
